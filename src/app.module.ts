e360d5d8649daae42058ada5760d8c67ebbb96bf
JUSTINECUTIIE <Justinepurisima1@gmail.com>
2024-09-07 15:04:11 +0800

    init

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostService } from './post/post.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { PostModule } from './post/post.module';
+
+@Module({
+  imports: [PrismaModule, PostModule],
+  controllers: [AppController],
+  providers: [AppService, PostService],
+})
+export class AppModule {}
